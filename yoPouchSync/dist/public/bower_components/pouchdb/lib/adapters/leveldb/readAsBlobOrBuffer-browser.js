bee3f755e17b5fd59ab246178e9c3feac687f545
Scott Nakada <scottnakada@gmail.com>
2015-08-14 18:53:00 -0700

    Add the distribution directory for the yoPouchSync project.  NOTE: the yoPouchSync/dist/public/index.html has been modified with paths relative to yoPouchSync/dist/public; so the web-page can be viewed directly from github.

--- yoPouchSync/dist/public/bower_components/pouchdb/lib/adapters/leveldb/readAsBlobOrBuffer-browser.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var binToBluffer = require('../../deps/binary/binaryStringToBlobOrBuffer');
-
-module.exports = function readAsBlobOrBuffer(storedObject, type) {
-  // In the browser, we've stored a binary string
-  return binToBluffer(storedObject, type);
-};
--- yoPouchSync/dist/public/bower_components/pouchdb/lib/deps/ajax/prequest.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var ajax = require('./ajax-core');
-
-module.exports = function(opts, callback) {
-  // do nothing; all the action is in prerequest-browser.js
-  return ajax(opts, callback);
-};
